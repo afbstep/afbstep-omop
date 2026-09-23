7a09475bc64ce3691a94ab446d306c9933a08a31
AFBSTEP CI <ci@noreply.local>
2026-09-23 13:21:04 +0000

    >Published from dev commit 486149ed

--- src/afbstepomop/analysis_tables/__init__.py
+++
@@ -1,4 +0,0 @@
-# src/afbstepomop/analysis_tables/__init__.py
-from .api import cdm_to_table
-
-__all__ = ["cdm_to_table"]
--- src/afbstepomop/analysis_tables/sql/discharge_index.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    o.person_id,
-    v.visit_end_date AS discharge_date
-FROM {schema}.observation AS o
-JOIN {schema}.visit_occurrence AS v
-  ON v.visit_occurrence_id = o.observation_event_id
-WHERE o.observation_concept_id = {baseline_visit_concept}
-  AND v.visit_end_date IS NOT NULL
